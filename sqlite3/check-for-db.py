ee4317c213b536a0ba46132c9e5355d2c11eca87
Vive-bit <77213543+Vive-bit@users.noreply.github.com>
2022-01-03 20:05:36 +0100

    Update check-for-db.py

--- sqlite3/check-for-db.py
+++ sqlite3/check-for-db.py
@@ -1,3 +1,21 @@
+
+import sqlite3 as sl
+import os
+
+
+DATABASE_PATH = "data/files/localdata.db"
+
+if not os.path.isfile('data/files/localdata.db'):
+  print(f"[ERROR] [sqlite3 database] database file ({DATABASE_PATH}) not found!")
+  print(f"[sqlite3 database] Creating database file in {DATABASE_PATH}")
+  pass
+
+DATABASE_LOCALDATE = sl.connect('{}'.format(DATABASE_PATH))
+DATABASE_LOCALDATE_CURSOR = DATABASE_LOCALDATE.cursor()
+
+
+
+
 def check_db_(name):
   listOfTables = DATABASE_LOCALDATE_CURSOR.execute(
   """SELECT name FROM sqlite_master WHERE type='table'
@@ -11,10 +29,49 @@ def create_db_table_(name):
   resl = check_db_(name)
   if resl == False:
     DATABASE_LOCALDATE_CURSOR.execute("""CREATE TABLE {} (id integer, type text)""".format(name))
+    DATABASE_LOCALDATE.commit()
     return True
   else:
     return False
 
+def db_add_(dbname, userid, typ):
+  g = check_db_(dbname)
+  if g == False:
+    return False
+  userid = int(userid)
+  typ = str(typ)
+  DATABASE_LOCALDATE_CURSOR.execute("INSERT INTO {} VALUES ({}, '{}')".format(dbname, userid, typ))
+  DATABASE_LOCALDATE.commit()
+  return True
 
-g = create_db_table_("asd")
+def db_remove_(dbname,userid,typ):
+  g = check_db_(dbname)
+  if g == False:
+    return False
+  userid = int(userid)
+  typ = str(typ)
+  DATABASE_LOCALDATE_CURSOR.execute("DELETE from {} WHERE id={} AND typ={}".format(dbname, userid, typ))
+  DATABASE_LOCALDATE.commit()
+  return True
+
+def db_listkey_(dbname,typ):
+  g = check_db_(dbname)
+  if g == False:
+    return False
+  typ = str(typ)
+  dblist = DATABASE_LOCALDATE_CURSOR.execute("SELECT * FROM {} WHERE type='{}'; ".format(dbname,typ)).fetchall()
+  return dblist
+
+d = 123 # ID
+typa = "tban" # TYPE
+nam = "asd" # TABLE NAME
+
+g = create_db_table_(nam)
 print(g)
+i = db_listkey_(nam,typa)
+print(i)
+a = db_add_(nam,d,typa)
+print(a)
+i = db_listkey_(nam,typa)
+print(i)
+
